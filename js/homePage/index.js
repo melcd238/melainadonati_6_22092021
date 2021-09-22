58f9492c1d7f1f1826179b7dc32c58779e30a094
melcd238 <melcd238@gmail.com>
2021-09-22 22:36:52 +0200

    adisplay photographers page index.html

--- js/homePage/index.js
+++ js/homePage/index.js
@@ -1,7 +1,39 @@
 import Header from './header.js';
 
-// seclector pour le Header
+// selector pour le Header
 const header = document.querySelector('.indexHeader');
 const headerContent = new Header(header);
 headerContent.createHeader();
 
+// Fetch
+fetch('data/data.json')
+  .then((response) => response.json())
+  .then((data) => {
+    const photographersArray = data.photographers;
+    const mediaArray = data.media;
+    console.log(mediaArray);
+
+    const sectionPhotographers = document.querySelector('#indexPage');
+
+    photographersArray.forEach((photographer) => {
+      const card = document.createElement('div');
+      card.setAttribute('class', 'cardPhotographer');
+      card.innerHTML = `   <a href="pages/photographer.html?id=${photographer.id}" class="linkPhotographer">
+            <img src="images/Portraits/${photographer.portrait}">
+            <h2>${photographer.name}</h2>
+       </a>
+       <p class="location"><span>${photographer.city}</span>, <span>${photographer.country}</span></p>
+      
+       <p class="textDesciption" role="text"> ${photographer.tagline}</p>
+       <p class="price">  ${photographer.price}€/jour</p>
+      
+       <div class="hastag">
+           <ul>
+               <li>#portrait</li>
+               <li>#events</li>
+               <li>#animal</li>
+           </ul>
+       </div>`;
+      sectionPhotographers.appendChild(card);
+    });
+  });
